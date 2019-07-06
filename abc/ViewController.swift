4a5936b18b72ea91a40d310261f6d0984889914a
quadmini2 <quadmini2@shivani-5.local>
2019-07-06 09:57:20 +0530

    Changed

--- abc/ViewController.swift
+++ abc/ViewController.swift
@@ -12,7 +12,7 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        print("Hello Swift Nishit")
+        print("Hello ... dfsfdf Swift Nishit")
 
         // Do any additional setup after loading the view.
     }
